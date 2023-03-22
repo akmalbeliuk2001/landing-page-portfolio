e1c84b3adec24d767b8759dde3e1b71ac01eea68
Muhammad Akmal Beliuk <akmalbeliuk@gmail.com>
2023-03-22 10:22:34 +0700

    [update] IntroducingSection.vue: update view for mobile

--- src/components/IntroducingSection.vue
+++ src/components/IntroducingSection.vue
@@ -4,12 +4,12 @@
       <div class="absolute left-[50%] translate-x-[-50%] top-[50%] translate-y-[-50%] w-full">
         <div class="flex justify-between items-center">
           <div>
-            <img class="w-[342px] h-[342px]" src="@/assets/akmal_head.png" alt="introducing">
+            <img class="w-[342px] h-[342px] hidden lg:visible" src="@/assets/akmal_head.png" alt="introducing">
           </div>
           <div>
-            <p class="poppins uppercase font-semibold text-[#000] text-[61.04px] leading-[100%]">Hi<br>There!</p>
-            <p class="nunito font-normal text-[#000] text-[25px] mt-8">My full name is <span class="text-[#F44F1A] font-bold">Muhammad Akmal Beliuk</span>.</p>
-            <p class="nunito font-normal text-[#000] text-[25px] max-w-[730px] mt-4">An informatics student who is interested in visual design. <span class="font-bold">Currently</span> focusing on improving skills in programming, especially for website development. <span class="font-bold">Skilled</span> in communication, critical thinking, creative thinking and leadership.</p>
+            <p class="poppins uppercase font-semibold text-[#000] text-[48.83px] lg:text-[61.04px] leading-[100%]">Hi<br>There!</p>
+            <p class="nunito font-normal text-[#000] text-[16px] lg:text-[25px] mt-8">My full name is <span class="text-[#F44F1A] font-bold">Muhammad Akmal Beliuk</span>.</p>
+            <p class="nunito font-normal text-[#000] text-[16px] lg:text-[25px] max-w-[730px] mt-4">An informatics student who is interested in visual design. <span class="font-bold">Currently</span> focusing on improving skills in programming, especially for website development. <span class="font-bold">Skilled</span> in communication, critical thinking, creative thinking and leadership.</p>
           </div>
         </div>
       </div>
