4e3fa326b678179885ca5b5894d918162b3c6490
Akmal <akmalbeliuk@gmail.com>
2023-10-01 23:18:12 +0700

    Starting revisi

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
   <navbar-section></navbar-section>
   <opening-section></opening-section>
-  <header-section></header-section>
+  <!-- <header-section></header-section>
   <introducing-section></introducing-section>
   <education-section></education-section>
   <quotes-section></quotes-section>
   <skill-section></skill-section>
   <contact-section></contact-section>
-  <footer-section></footer-section>
+  <footer-section></footer-section> -->
 </template>
 
 <script setup>
 import NavbarSection from "./components/NavbarSection.vue";
 import OpeningSection from "./components/OpeningSection.vue";
-import HeaderSection from "./components/HeaderSection.vue";
-import IntroducingSection from "@/components/IntroducingSection.vue";
-import EducationSection from "@/components/EducationSection.vue";
-import QuotesSection from "@/components/QuotesSection.vue";
-import SkillSection from "@/components/SkillSection.vue";
-import ContactSection from "./components/ContactSection.vue";
-import FooterSection from "./components/FooterSection.vue";
+// import HeaderSection from "./components/HeaderSection.vue";
+// import IntroducingSection from "@/components/IntroducingSection.vue";
+// import EducationSection from "@/components/EducationSection.vue";
+// import QuotesSection from "@/components/QuotesSection.vue";
+// import SkillSection from "@/components/SkillSection.vue";
+// import ContactSection from "./components/ContactSection.vue";
+// import FooterSection from "./components/FooterSection.vue";
 </script>
 
 
--- src/components/NavbarSection.vue
+++ src/components/NavbarSection.vue
@@ -1,15 +1,37 @@
 <template>
-  <div class="navbar fixed z-[99] bg-[#F44F1A] h-[60px] w-full border-b-[1px] border-solid border-[#000]">
+  <div :style="{'border-color' : borderColor}" :class="changeBackgroundColor ? 'bg-[#F44F1A]' : 'bg-[#FFF]'" class="navbar fixed z-[99] h-[60px] w-full border-b-[1px] border-solid transition-all duration-500">
     <div class="mx-auto px-4 md:px-7 lg:px-0 lg:max-w-[1064px] overflow-hidden">
       <div class="flex nunito font-normal text-xs lg:text-base justify-between items-center pt-[16px] lg:pt-[13px]">
-        <p class="border-[1px] border-[#000] rounded-[30px] px-[8px] py-[4px]">Portfolio</p>
-        <p class="border-[1px] border-[#000] rounded-[30px] px-[8px] py-[4px]">2023</p>
+        <p :style="{'border-color' : borderColor, color : borderColor}" class="border-[1px] rounded-[30px] px-[8px] py-[4px]">Portfolio</p>
+        <p :style="{'border-color' : borderColor, color : borderColor}" class="border-[1px] rounded-[30px] px-[8px] py-[4px]">2023</p>
       </div>
     </div>
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref,onMounted, onUnmounted } from 'vue';
+
+const changeBackgroundColor = ref(false)
+const borderColor = ref('#F44F1A')
+
+const handleScroll = () => {
+  if (window.scrollY > 300) {
+    changeBackgroundColor.value = true
+    borderColor.value = '#FFF'
+  } else {
+    changeBackgroundColor.value = false
+    borderColor.value = '#F44F1A'
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+}) 
 </script>
 
 <style>
