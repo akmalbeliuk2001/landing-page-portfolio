a3ab8e800cb9e3e289656f715f1770dae7093d34
Akmal <akmalbeliuk@gmail.com>
2023-10-03 00:58:31 +0700

    Adding decoration in introduction section

--- src/components/IntroducingSection.vue
+++ src/components/IntroducingSection.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="introducing bg-[#000] h-[130vh] lg:h-screen">
+  <div class="introducing relative bg-[#000] h-[130vh] lg:h-screen">
     <div
-      class="relative mx-4 md:mx-7 lg:mx-auto lg:max-w-[1064px] h-full flex justify-center items-center flex-col"
+      class="mx-4 md:mx-7 lg:mx-auto lg:max-w-[1064px] h-full flex justify-center items-center flex-col"
     >
       <div
         class="flex justify-between items-center flex-col lg:flex-row w-full gap-10"
@@ -52,6 +52,19 @@
         />
       </svg>
     </div>
+    <svg
+      class="rotate absolute top-[-15%] right-[-6%] hidden lg:block"
+      width="300"
+      height="300"
+      viewBox="0 0 227 226"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M113.5 0L121.982 57.4519L147.049 5.04849L138.192 62.444L177.616 19.7453L152.209 71.9846L202.487 42.7846L162.785 85.226L219.451 72.1193L168.983 100.992L227 105.142L170.25 117.88L224.465 138.92L166.476 134.393L212.07 170.452L157.994 149.06L190.916 196.935L145.558 160.579L162.884 216.016L130.275 167.928L130.464 226L113.5 170.452L96.5362 226L96.7259 167.928L64.1162 216.016L81.4422 160.579L36.084 196.935L69.0068 149.06L14.9305 170.452L60.5249 134.393L2.53537 138.92L56.7501 117.88L0 105.142L58.0178 100.992L7.54954 72.1193L64.2154 85.226L24.5134 42.7846L74.7918 71.9846L49.384 19.7453L88.8083 62.444L79.9516 5.04849L105.019 57.4519L113.5 0Z"
+        fill="#F44F1A"
+      />
+    </svg>
   </div>
 </template>
 
